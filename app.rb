9dbc120f9e67b40c83223fb3480fb74c392cee38
Ronda D Cowen <cowenhome5@gmail.com>
2020-04-20 21:12:58 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,7 +2,13 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do
+  get '/name' do
+  "Hello, World!"
+end
+get '/hometown' do
+  "Hello, World!"
+end
+get '/favorite' do
   "Hello, World!"
 end
 end
